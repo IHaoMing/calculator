927adfbf54d14fefb4985ac616962b5e72f13069
IHaoMing <haoming.wu168@gmail.com>
2017-11-03 09:50:35 +0800

    使用模板

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -1,6 +1,6 @@
 //------------------------
-// 简答计算器v1.0
-// date:2017-11-02
+// 简答计算器v1.10
+// date:2017-11-03
 //------------------------
 #include <iostream>
 #include<string>
@@ -8,11 +8,36 @@
 using namespace std;
 
 void help();
+/*
 int calculator(int a, char op, int b);//整数四则运算
 double calculator(double a, char op, double b);//浮点数
 double calculator(double a, char op, int b);
 double calculator(int a, char op, double b);
 
+*/
+template<typename T>
+T calculator(T a, char op, T b)
+{
+    T result;
+    switch(op)
+    {
+        case '+':
+			result = a + b;
+		break;
+		case '-':
+			result = a - b;
+		break;
+		case '*':
+			result = a * b;
+		break;
+		case '/':
+			result = a / b;
+		break;
+		default:
+		break;
+    }
+    return result;
+}
 int main()
 {
     help();
@@ -30,98 +55,6 @@ int main()
 void help()
 {
     cout<<"简单计算器："<<endl;
-}
-
-int calculator(int a, char op, int b)
-{
-    int result = 0;
-    switch(op)
-    {
-        case '+':
-			result = a + b;
-		break;
-		case '-':
-			result = a - b;
-		break;
-		case '*':
-			result = a * b;
-		break;
-		case '/':
-			result = a / b;
-		break;
-		default:
-		break;
-    }
-    return result;
-}
-
-double calculator(double a, char op, double b)
-{
-    double result = 0.0;
-    switch(op)
-    {
-        case '+':
-			result = a + b;
-		break;
-		case '-':
-			result = a - b;
-		break;
-		case '*':
-			result = a * b;
-		break;
-		case '/':
-			result = a / b;
-		break;
-		default:
-		break;
-    }
-    return result;
-}
-
-double calculator(int a, char op, double b)
-{
-    double result = 0.0;
-    switch(op)
-    {
-        case '+':
-			result = a + b;
-		break;
-		case '-':
-			result = a - b;
-		break;
-		case '*':
-			result = a * b;
-		break;
-		case '/':
-			result = a / b;
-		break;
-		default:
-		break;
-    }
-    return result;
-}
-
-double calculator(double a, char op, int b)
-{
-    double result = 0.0;
-    switch(op)
-    {
-        case '+':
-			result = a + b;
-		break;
-		case '-':
-			result = a - b;
-		break;
-		case '*':
-			result = a * b;
-		break;
-		case '/':
-			result = a / b;
-		break;
-		default:
-		break;
-    }
-    return result;
 }
 
 
